204618a15860150a804e0a8bc7ebc450e76c785a
Flild <flildman@yandex.ru>
2024-06-19 23:34:06 +0300

    commit

--- Cosmo.Web/Controllers/CouponController.cs
+++ Cosmo.Web/Controllers/CouponController.cs
@@ -2,6 +2,7 @@
 using Cosmo.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Cosmo.Web.Controllers
 {
@@ -29,5 +30,30 @@ namespace Cosmo.Web.Controllers
         {
             return View();
         }
-    }
+
+        [HttpPost]
+		public async Task<IActionResult> CouponCreate(CouponDto model)
+		{
+            if (ModelState.IsValid)
+            {
+				ResponseDto? responce = await _couponService.CreateCouponAsync(model);
+				if (responce != null && responce.IsSuccess)
+				{
+					return RedirectToAction("Index", "Home");
+				}
+			}
+			return View(model);
+		}
+
+		public async Task<IActionResult> CouponDelete(int CouponID)
+		{
+			ResponseDto? responce = await _couponService.GetCouponByIdAsync(CouponID);
+			if (responce != null && responce.IsSuccess)
+			{
+				CouponDto? model = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(responce.Result));
+                return View(model);
+			}
+			return NotFound();
+		}
+	}
 }
