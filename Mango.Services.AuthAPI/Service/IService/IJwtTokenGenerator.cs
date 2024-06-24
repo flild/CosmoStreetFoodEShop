bc00bda318b3bb111ef0995bb513d6a442b2d131
Flild <flildman@yandex.ru>
2024-06-24 11:31:08 +0300

    add jwt token

---
+++ Mango.Services.AuthAPI/Service/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using Cosmo.Services.AuthAPI.Models;
+
+namespace Cosmo.Services.AuthAPI.Service.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser);
+    }
+}
