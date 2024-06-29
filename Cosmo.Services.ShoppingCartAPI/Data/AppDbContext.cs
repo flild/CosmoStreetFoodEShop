51b254cc0494cc475e3da86efdf737665d8bac86
Flild <flildman@yandex.ru>
2024-06-30 00:56:37 +0300

    add shopping car API

---
+++ Cosmo.Services.ShoppingCartAPI/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Cosmo.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Cosmo.Services.ShoppingCartAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
---
+++ Cosmo.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Cosmo.Services.ShoppingCartAPI.Models;
+using Cosmo.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Cosmo.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration( config =>
+            {
+                config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
