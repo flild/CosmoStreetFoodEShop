132631f3d2d6a7e71e36b1db6b0142e93cbc76e6
Flild <flildman@yandex.ru>
2024-07-02 00:34:40 +0300

    add cartService in web

---
+++ Cosmo.Web/Service/CartService.cs
@@ -0,0 +1,54 @@
+﻿using Cosmo.Web.Models;
+using Cosmo.Web.Service.IService;
+using Cosmo.Web.Utility;
+
+namespace Cosmo.Web.Service
+{
+    public class CartService : ICartService
+    {
+        private readonly IBaseService _baseService;
+        public CartService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDto?> ApplyCouponAsync(CartDto cartDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                Apitype = SD.ApiType.POST,
+                Data = cartDto,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/ApplyCoupon",
+            });
+        }
+
+        public async Task<ResponseDto?> GetCartByUserIdAsync(string userId)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                Apitype = SD.ApiType.POST,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/GetCart/" + userId,
+            });
+        }
+
+        public async Task<ResponseDto?> RemoveFromCartAsync(int cartDetailsId)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                Apitype = SD.ApiType.POST,
+                Data = cartDetailsId,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/RemoveCart/",
+            });
+        }
+
+        public async Task<ResponseDto?> UpsertCartAsync(CartDto cartDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                Apitype = SD.ApiType.POST,
+                Data = cartDto,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/Cartupsert",
+            });
+        }
+    }
+}
