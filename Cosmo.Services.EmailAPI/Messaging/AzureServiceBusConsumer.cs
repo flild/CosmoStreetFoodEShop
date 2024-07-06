a6eaed5084835de4de4b3eec58081759e0c4784a
Flild <flildman@yandex.ru>
2024-07-07 00:20:10 +0300

    add service bus

---
+++ Cosmo.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,66 @@
+﻿using Azure.Messaging.ServiceBus;
+using Cosmo.Services.EmailAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore.Storage.Json;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Cosmo.Services.EmailAPI.Messaging
+{
+    public class AzureServiceBusConsumer : IAzureServiceBusConsumer
+    {
+        private readonly string ServiceBusConnectionString;
+        private readonly string EmailCartQueue;
+        private readonly IConfiguration _config;
+
+        private ServiceBusProcessor _emailCartProcessor;
+
+        public AzureServiceBusConsumer(IConfiguration config)
+        {
+            _config = config;
+            ServiceBusConnectionString = _config.GetValue<string>("ServiceBusConnectionString");
+            EmailCartQueue = _config.GetValue<string>("TopicAndQueueNames:EmailShoppingCartQueue");
+
+            var client = new ServiceBusClient(ServiceBusConnectionString);
+            _emailCartProcessor = client.CreateProcessor(EmailCartQueue);
+
+
+        }
+
+        public async Task Start()
+        {
+            _emailCartProcessor.ProcessMessageAsync += OnEmailCartRequestReceived;
+            _emailCartProcessor.ProcessErrorAsync += ErrorHandler;
+        }
+
+        private Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task OnEmailCartRequestReceived(ProcessMessageEventArgs args)
+        {
+            //this is where you will receive the message
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            CartDto objMessage = JsonConvert.DeserializeObject<CartDto>(body);
+            try
+            {
+                //todo try to log email
+                await args.CompleteMessageAsync(args.Message);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task Stop()
+        {
+            await _emailCartProcessor.StopProcessingAsync();
+            await _emailCartProcessor.DisposeAsync();
+        }
+    }
+}
