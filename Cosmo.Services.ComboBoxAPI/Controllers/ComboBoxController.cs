ec6a928b679fddf53183e18d7ee71d6fbbca8c92
Flild <flildman@yandex.ru>
2024-08-11 02:10:06 +0300

    add new service for combo

---
+++ Cosmo.Services.ComboBoxAPI/Controllers/ComboBoxController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cosmo.Services.ComboBoxAPI.Controllers
+{
+    [Route("api/combobox")]
+    [ApiController]
+    public class ComboBoxController : ControllerBase
+    {
+    }
+}
