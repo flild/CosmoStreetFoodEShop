f6133004d7ca0ba02764a5f626ba1453a03d0b82
Flild <flildman@yandex.ru>
2024-09-13 03:55:39 +0300

    add some unit tests

---
+++ CosmoStreetFoodProject.Tests/FrontEnd/Controllers/ProductControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using FakeItEasy;
+using Cosmo.Web.Service.IService;
+using Cosmo.Web.Controllers;
+using Cosmo.Web.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CosmoStreetFoodProject.Tests.FrontEnd.Controllers
+{
+    public class ProductControllerTests
+    {
+        private readonly IProductService _productService;
+        private readonly ProductController _productController;
+
+        public ProductControllerTests()
+        {
+            _productService = A.Fake<IProductService>();
+
+            //SUT
+            _productController = new ProductController(_productService);
+        }
+        [Fact]
+        public void ProductController_ProductIndex_ReturnSuccess()
+        {
+            //Arrange
+            var products = A.Fake<ProductDto>();
+            //Act
+            var result = _productController.ProductIndex();
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+    }
+}
--- CosmoStreetFoodProject.Tests/Services/ProductAPI/ProductApiControllerTests.cs
+++ CosmoStreetFoodProject.Tests/Services/ProductAPI/ProductApiControllerTests.cs
@@ -1,20 +1,33 @@
-﻿using Cosmo.Services.ProductAPI.Services;
+﻿using Cosmo.Services.ProductAPI.Controllers;
+using Cosmo.Services.ProductAPI.Models.Dto;
+using Cosmo.Services.ProductAPI.Services;
 using FakeItEasy;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FluentAssertions;
 
 namespace CosmoStreetFoodProject.Tests.Services.ProductAPI
 {
-    internal class ProductApiControllerTests
+    public class ProductApiControllerTests
     {
         private readonly IProductService _productService;
+        private readonly ProductAPIController _productController;
 
         public ProductApiControllerTests()
         {
             _productService = A.Fake<IProductService>();
+
+            //SUT
+            _productController = new ProductAPIController(_productService);
+        }
+        [Fact]
+        public void ProductApiController_Get_ReturnSuccess()
+        {
+            //Arrang
+            var products = A.Fake<ResponseDto>();
+            A.CallTo(() => _productService.GetAllProducts()).Returns(products);
+            //Act
+            var result = _productController.Get();
+            //Assert
+            result.Should().BeOfType<ResponseDto>();
         }
     }
 }
